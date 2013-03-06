5f7336cfaf207bf9fd5489b98318739555881f6d
Alexey <leopard.not.a@gmail.com>
2013-03-07 00:58:54 +0200

    ext

--- lib/webp_ffi.rb
+++ lib/webp_ffi.rb
@@ -1,5 +1,14 @@
+require 'ffi'
+
 require "webp_ffi/version"
 
 module WebpFfi
-  # Your code goes here...
+  extend FFI::Library
+
+  # Try loading in order.
+  library = File.dirname(__FILE__) + "/../ext/webp_ffi/webp_ffi"
+  candidates = ['.bundle', '.so', '.dylib', ''].map { |ext| library + ext }
+  ffi_lib(candidates)
+
+  attach_function :test, [:int], :int
 end
