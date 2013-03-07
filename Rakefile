e5aeb564253eb723106c95caf286b239561ae9a2
Alexey <leopard.not.a@gmail.com>
2013-03-07 02:14:53 +0200

    fixes

--- Rakefile
+++ Rakefile
@@ -1,1 +1,11 @@
 require "bundler/gem_tasks"
+
+require 'ffi-compiler/export_task'
+
+def gem_spec
+  @gem_spec ||= Gem::Specification.load('webp_ffi.gemspec')
+end
+
+FFI::Compiler::ExportTask.new('lib', 'ext', :gem_spec => gem_spec) do |t|
+  t.export 'webp_ffi.rb'
+end
--- lib/webp_ffi.rb
+++ lib/webp_ffi.rb
@@ -5,12 +5,7 @@ require "webp_ffi/version"
 
 module WebpFfi
   extend FFI::Library
-
-  # Try loading in order.
-  #library = File.dirname(__FILE__) + "/../ext/webp_ffi/webp_ffi"
-  #candidates = ['.bundle', '.so', '.dylib', ''].map { |ext| library + ext }
-  #ffi_lib(candidates)
-  ffi_lib FFI::Compiler::Loader.find('web_ffi')
+  ffi_lib FFI::Compiler::Loader.find('webp_ffi')
 
   attach_function :test, [:int], :int
 end
