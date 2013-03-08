93b62be1e1c05a04df2bdc1edae05f0b171bbdb0
Alexey Vasiliev <leopard.not.a@gmail.com>
2013-03-08 03:16:49 +0200

    fix

--- ext/webp_ffi/webp_ffi.c
+++ ext/webp_ffi/webp_ffi.c
@@ -1,29 +1,3 @@
-#include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-// libwebp
-#include "webp/decode.h"
-#include "webp/encode.h"
-
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#endif
-
-#ifdef WEBP_HAVE_PNG
-#include <png.h>
-#endif
-
-#ifdef WEBP_HAVE_JPEG
-#include <setjmp.h>   // note: this must be included *after* png.h
-#include <jpeglib.h>
-#endif
-
-#ifdef WEBP_HAVE_TIFF
-#include <tiffio.h>
-#endif
-
-
 #include "webp_ffi.h"
 
 
--- ext/webp_ffi/webp_ffi.h
+++ ext/webp_ffi/webp_ffi.h
@@ -1,3 +1,27 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+// libwebp
+#include "webp/decode.h"
+#include "webp/encode.h"
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#ifdef WEBP_HAVE_PNG
+#include <png.h>
+#endif
+
+#ifdef WEBP_HAVE_JPEG
+#include <setjmp.h>   // note: this must be included *after* png.h
+#include <jpeglib.h>
+#endif
+
+#ifdef WEBP_HAVE_TIFF
+#include <tiffio.h>
+#endif
 // utils
 #include "./util.h"
 
@@ -5,6 +29,8 @@
 #if defined(__cplusplus) || defined(c_plusplus)
 extern "C" {
 #endif
+
+int WebPGetInfo(const uint8_t* data, size_t data_size, int* width, int* height);
 
 int test(int n);
 
