c461c90da7608ef76131a62220be0d30ad811531
Alexey Vasiliev <leopard.not.a@gmail.com>
2013-03-08 03:08:41 +0200

    move

---
+++ lib/webp_ffi/webp.rb
@@ -0,0 +1,21 @@
+require "webp_ffi/c"
+
+module WebpFfi
+  module Webp
+    # get webp image size
+    def webp_size(data)
+      return nil if data.nil?
+      width_ptr = FFI::MemoryPointer.new(:int, 2)
+      height_ptr = FFI::MemoryPointer.new(:int, 2)
+      size = data.respond_to?(:bytesize) ? data.bytesize : data.size
+      memBuf = FFI::MemoryPointer.new(:char, size)
+      memBuf.put_bytes(0, data)
+      if WebpFfi::C.WebPGetInfo(memBuf, size, width_ptr, height_ptr) == 1
+        [width_ptr.read_int, height_ptr.read_int]
+      else
+        return nil
+      end
+    end
+    
+  end
+end
