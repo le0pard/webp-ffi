95385d57dba9abc4477d2196c6ebeaa7ec87a303
Alexey Vasiliev <leopard.not.a@gmail.com>
2013-03-11 16:29:05 +0200

    cleanup

--- lib/webp_ffi/webp_ffi.rb
+++ lib/webp_ffi/webp_ffi.rb
@@ -19,11 +19,9 @@ module WebpFfi
     # get webp image size
     def webp_size(data)
       return nil if data.nil?
-      size = data.respond_to?(:bytesize) ? data.bytesize : data.size
-      width_ptr, height_ptr = FFI::MemoryPointer.new(:int, 2), FFI::MemoryPointer.new(:int, 2)
-      memBuf = FFI::MemoryPointer.new(:char, size).put_bytes(0, data)
-      if 0 == C.webp_get_info(memBuf, size, width_ptr, height_ptr)
-        [(width_ptr.null? ? nil : width_ptr.read_int), (height_ptr.null? ? nil : height_ptr.read_int)]
+      pointers = get_pointers_for_webp_size(data)
+      if 0 == C.webp_get_info(pointers[:data], pointers[:data_size], pointers[:width], pointers[:height])
+        [(pointers[:width].null? ? nil : pointers[:width].read_int), (pointers[:height].null? ? nil : pointers[:height].read_int)]
       else
         raise InvalidImageFormatError, "invalid WebP image data"
       end
@@ -41,6 +39,16 @@ module WebpFfi
       res = C.webp_encode(input_file, output_file, options_obj.encode_pointer)
       raise EncoderError, ENCODER_ERRORS[res - 1] unless 0 == res
       return true
+    end
+    
+    private
+    
+    def get_pointers_for_webp_size(data)
+      pointers = Hash.new
+      pointers[:data_size] = data.respond_to?(:bytesize) ? data.bytesize : data.size
+      pointers[:width], pointers[:height] = FFI::MemoryPointer.new(:int, 2), FFI::MemoryPointer.new(:int, 2)
+      pointers[:data] = FFI::MemoryPointer.new(:char, pointers[:data_size]).put_bytes(0, data)
+      pointers
     end
     
   end
