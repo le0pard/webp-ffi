210e7c16d24bcb062f27ee871a45fb434f098271
Alexey Vasiliev <leopard.not.a@gmail.com>
2014-12-08 12:32:26 +0200

    fix build

--- spec/travis_build.sh
+++ spec/travis_build.sh
@@ -1,7 +1,7 @@
 #/usr/bin/env sh
-wget http://downloads.webmproject.org/releases/webp/libwebp-0.4.2.tar.gz
-tar xvzf libwebp-0.4.2.tar.gz
-cd libwebp-0.4.2
+wget https://webp.googlecode.com/files/libwebp-0.4.0.tar.gz
+tar xvzf libwebp-0.4.0.tar.gz
+cd libwebp-0.4.0
 ./configure
 make && sudo make install
 sudo ln -fs /usr/local/lib/libwebp.* /usr/lib/
