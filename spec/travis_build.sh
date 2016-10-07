958831841ea632947967754c62aafd5cae33d279
Alexey Vasiliev <leopard.not.a@gmail.com>
2016-10-07 22:42:30 +0300

    new webp

--- spec/travis_build.sh
+++ spec/travis_build.sh
@@ -1,6 +1,6 @@
 #/usr/bin/env sh
-wget http://downloads.webmproject.org/releases/webp/libwebp-0.4.3.tar.gz
-tar xvzf libwebp-0.4.3.tar.gz
-cd libwebp-0.4.3
+wget http://downloads.webmproject.org/releases/webp/libwebp-0.5.1.tar.gz
+tar xvzf libwebp-0.5.1.tar.gz
+cd libwebp-0.5.1
 ./configure --prefix=$HOME/opt
 make && make install
