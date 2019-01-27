9cc4747cda55f5c03f91c063cf44a8f3c0662063
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2019-01-27 13:17:02 +0100

    Update travis_build.sh to libwebp 1.0.2

--- spec/travis_build.sh
+++ spec/travis_build.sh
@@ -1,6 +1,6 @@
 #/usr/bin/env sh
-wget http://downloads.webmproject.org/releases/webp/libwebp-0.5.1.tar.gz
-tar xvzf libwebp-0.5.1.tar.gz
-cd libwebp-0.5.1
+wget http://downloads.webmproject.org/releases/webp/libwebp-1.0.2.tar.gz
+tar xvzf libwebp-1.0.2.tar.gz
+cd libwebp-1.0.2
 ./configure --prefix=$HOME/opt
 make && make install
