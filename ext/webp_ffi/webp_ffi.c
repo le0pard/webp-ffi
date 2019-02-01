ef2ff7d0f15ae00a30e669105d8689d649f912cc
Alexey Vasiliev <leopard.not.a@gmail.com>
2019-02-01 17:06:56 +0200

    testing different versions

--- ext/webp_ffi/webp_ffi.c
+++ ext/webp_ffi/webp_ffi.c
@@ -64,7 +64,7 @@ int webp_encode(const char *in_file, const char *out_file, const FfiWebpEncodeCo
   if (encode_config->lossless == 0 || encode_config->lossless == 1){
     config.lossless = encode_config->lossless;
   }
-  if (encode_config->near_lossless >= 0 && encode_config->near_lossless <= 100){
+  if (WebPGetEncoderVersion() >= 0x010000 && encode_config->near_lossless >= 0 && encode_config->near_lossless <= 100){
     config.near_lossless = encode_config->near_lossless;
     config.lossless = 1;  // use near-lossless only with lossless
   }
--- spec/travis_build.sh
+++ spec/travis_build.sh
@@ -1,6 +1,6 @@
 #/usr/bin/env sh
-wget http://downloads.webmproject.org/releases/webp/libwebp-1.0.2.tar.gz
-tar xvzf libwebp-1.0.2.tar.gz
-cd libwebp-1.0.2
+wget http://downloads.webmproject.org/releases/webp/libwebp-$LIBWEBP_VERSION.tar.gz
+tar xvzf libwebp-$LIBWEBP_VERSION.tar.gz
+cd libwebp-$LIBWEBP_VERSION
 ./configure --prefix=$HOME/opt
 make && make install
